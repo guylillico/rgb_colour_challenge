d0ce9230f737b04aa80af75f441099a5d08035c7
Guy Lillico <guylillico@gmail.com>
2022-08-06 00:26:49 +0800

    Setting array length to a variable

--- src/components/ColourCanvas/ColourCanvas.jsx
+++ src/components/ColourCanvas/ColourCanvas.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { ColourCanvasWrapper, ColourCanvasDesc } from "./ColourCanvas.styled"
-import { generateColourArray, shuffleColours, maxMinColours, drippyColours } from "../../util/colour"
+import { generateColourArray, shuffleColours, maxMinColours, drippyColours, newOne } from "../../util/colour"
 
 /**
  * ColourCanvas is a component that displays a canvas with 32,768 dicrete colours
@@ -16,25 +16,30 @@ const ColourCanvas = ({ desc, effect }) => {
   const colourCanvasRef = React.useRef(null)
 
   React.useEffect(() => {
-    const canvasElement = colourCanvasRef.current
+    const canvasElement = colourCanvasRef?.current
     const context = canvasElement.getContext("2d")
 
-    canvasElement.width = canvasWidth
-    canvasElement.height = canvasHeight
+    if (canvasElement) {
+      canvasElement.width = canvasWidth
+      canvasElement.height = canvasHeight
+    }
 
     let coloursArray = generateColourArray()
+    let numColours = coloursArray.length
 
     if (effect === "static") shuffleColours(coloursArray)
-    if (effect === "bars") maxMinColours(coloursArray, coloursArray.length)
+    if (effect === "bars") maxMinColours(coloursArray, numColours)
     if (effect === "drippy") drippyColours(coloursArray)
 
-    // Draw colours on the canvas
-    let i = 0
-    for (let x = 0; x < canvasWidth; x++) {
-      for (let y = 0; y < canvasHeight; y++) {
-        context.fillStyle = `rgb(${coloursArray[i].r}, ${coloursArray[i].g}, ${coloursArray[i].b})`
-        context.fillRect(x, y, 1, 1)
-        i++
+    if (context) {
+      // Draw colours on the canvas
+      let i = 0
+      for (let x = 0; x < canvasWidth; x++) {
+        for (let y = 0; y < canvasHeight; y++) {
+          context.fillStyle = `rgb(${coloursArray[i].r}, ${coloursArray[i].g}, ${coloursArray[i].b})`
+          context.fillRect(x, y, 1, 1)
+          i++
+        }
       }
     }
   }, [])
