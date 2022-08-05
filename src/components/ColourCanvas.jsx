ef857697ab41d466f8ad8a17c16e4a9df0739e88
Guy Lillico <guylillico@gmail.com>
2022-08-05 17:25:53 +0800

    Applying colours to the canvas to generate the image

--- src/components/ColourCanvas.jsx
+++ src/components/ColourCanvas.jsx
@@ -6,14 +6,25 @@ const ColourCanvas = ({ width, height }) => {
 
   React.useEffect(() => {
     const canvasElement = colourCanvasRef.current
+    const context = canvasElement.getContext("2d")
 
     canvasElement.width = width
     canvasElement.height = height
 
     let coloursArray = generateColourArray()
+
+    // Draw colours on the canvas
+    let i = 0
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        context.fillStyle = `rgb(${coloursArray[i].r}, ${coloursArray[i].g}, ${coloursArray[i].b})`
+        context.fillRect(x, y, 1, 1)
+        i++
+      }
+    }
   }, [])
 
-  return <canvas ref={colourCanvasRef} style={{ border: "1px solid #000" }} />
+  return <canvas ref={colourCanvasRef} />
 }
 
 export default ColourCanvas
